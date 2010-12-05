624165631c9126e2531aff0febbd779b47f8eb16
William Howard <whoward.tke@gmail.com>
2010-12-05 17:33:32 -0500

    added queue for messages while logger client is connecting

--- logger_client.js
+++ logger_client.js
@@ -12,8 +12,23 @@ LoggerClient = function(port) {
   // assign default values to the optional port argument
   port = port || 9000;
   
+  /** the queue of messages to process once connected */
+  this.afterConnectQueue = [];
+  
   /** socket object for communicating with the remote plugin */
   this.socket = new QTcpSocket(null);
+  
+  /** @ignore */
+  var self = this;
+  this.socket.connected.connect(function() { 
+    self.processQueue();
+    self.afterConnectQueue = null;
+  });
+  this.socket.error.connect(function() {
+    self.afterConnectQueue = null;
+  });
+  
+  // connect to the server immediately
   this.socket.connectToHost("127.0.0.1", port);
 };
 
@@ -29,16 +44,35 @@ LoggerClient.protocol = {
   "fieldSeparator": String.fromCharCode(31),
 }
 
+/**
+ * Processes all messages in the connection queue
+ * @private
+ */
+LoggerClient.prototype._processQueue = function() {
+  for(var i = 0; i < this.afterConnectQueue.length; i++) {
+    this._callRemote.apply(this, this.afterConnectQueue[i]);
+  }
+};
+
 LoggerClient.prototype.error = function(message) {
-  this._callRemote("error", message);
+  this._callRemoteWithConnectQueue("error", message);
 };
 
 LoggerClient.prototype.notify = function(message) {
-  this._callRemote("notify", message);
+  this._callRemoteWithConnectQueue("notify", message);
 };
 
 LoggerClient.prototype.log = function(message) {
-  this._callRemote("log", message);
+  this._callRemoteWithConnectQueue("log", message);
+};
+
+LoggerClient.prototype._callRemoteWithConnectQueue = function() {
+  if(this.afterConnectQueue && !this.socket.isValid()) {
+    this.afterConnectQueue.push(arguments);
+    return;
+  }
+  
+  this._callRemote.apply(this, arguments);
 };
 
 LoggerClient.prototype._callRemote = function(remote, message) {
